2d33d9e65244652f4c2147a4ccb4cdd4b21bff83
Hieu <xuanhieu0012@gmail.com>
2021-08-09 22:58:09 -0500

    commit tree 13556f3274aad237a82b8b0dcf255c59b3dc0c71

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'anyone except Bob';
+
+function upperCaseCustomerName(){
+    customerName= customerName.toUpperCase();
+    //return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer= 'not bob';
+    //return setBestCustomer;
+    
+}
+
+function overwriteBestCustomer(){
+    bestCustomer= 'maybe bob';
+    //return overwriteBestCustomer;
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer ='Poor Bob';
+}
